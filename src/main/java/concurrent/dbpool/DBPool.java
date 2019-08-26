afaf943f28e9a535f422cd63e586e681f3300040
nianjie.chen <15663456952@163.com>
2019-08-26 17:46:13 +0800

    多线程测试连接池

---
+++ src/main/java/concurrent/dbpool/DBPool.java
@@ -0,0 +1,60 @@
+package concurrent.dbpool;
+
+import java.sql.Connection;
+import java.util.LinkedList;
+
+/**
+ * 数据库连接池
+ * 具有延迟等待的机制
+ */
+public class DBPool {
+
+    //创建存储连接的容器
+    static LinkedList<Connection> pool = new LinkedList<>();
+
+    public DBPool(int initPools) {
+        for (int i = 0; i < initPools; i++) {
+            pool.add(SqlConnectImpl.fetchConnection());
+        }
+    }
+
+    /**
+     * 获取连接
+     * @param ms 等待时间，单位ms
+     * @return
+     */
+    public static Connection fetchConnection(int ms) throws InterruptedException {
+        synchronized (pool) {
+            if (ms <= 0) {//等待时间小于0时候
+                //判断连接池中是否含有连接，加锁
+                while (pool.isEmpty()) {
+                    pool.wait();
+                }
+                return pool.getFirst();
+            }else {
+                long overTime = ms + System.currentTimeMillis();//超时时间 = 等待时间 + 当前时间
+                long waitTime = ms;
+                //有等待时间和连接池为空时等待
+                while (pool.isEmpty() && waitTime > 0){
+                    pool.wait();
+                    waitTime = overTime - System.currentTimeMillis();
+                }
+                //如果连接池有则返回一个连接，否则返回空
+                if (pool.isEmpty()) {
+                    return null;
+                }
+                return pool.getFirst();
+            }
+        }
+    }
+
+    /**
+     * 释放一个连接
+     * @param conn
+     */
+    public static void release(Connection conn) {
+        if (conn!=null) {
+            pool.addLast(conn);
+        }
+    }
+}
---
+++ src/main/java/concurrent/dbpool/DBPoolTest.java
@@ -0,0 +1,76 @@
+package concurrent.dbpool;
+
+import java.sql.SQLException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 连接池测试类
+ * 多线程测试
+ */
+public class DBPoolTest {
+
+    static private CountDownLatch countDownLatch;
+
+    public DBPoolTest(CountDownLatch countDownLatch) {
+        this.countDownLatch = countDownLatch;
+    }
+
+    static class Worker implements Runnable{
+
+        //线程执行次数
+        private int count;
+        //线程安全的技术器（记录获取到的连接和未获取到的连接个数）
+        private AtomicInteger gets;
+        private AtomicInteger noGets;
+
+        public Worker(int count, AtomicInteger gets, AtomicInteger noGets) {
+            this.count = count;
+            this.gets = gets;
+            this.noGets = noGets;
+        }
+
+        @Override
+        public void run() {
+            while (count > 0) {
+                try {
+                    SqlConnectImpl connection = (SqlConnectImpl)DBPool.fetchConnection(100);
+                    if (connection==null) {
+                        noGets.getAndIncrement();
+                        System.out.println(Thread.currentThread().getName() + "：{}获取连接失败");
+                    }else {
+                        System.out.println(Thread.currentThread().getName() + "获取连接成功【"+count+"】");
+                        try {
+                            gets.getAndIncrement();
+                            connection.commit();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }finally {
+                            DBPool.release(connection);
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    count--;
+                }
+            }
+            countDownLatch.countDown();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        DBPool dbPool = new DBPool(10);
+        CountDownLatch countDownLatch = new CountDownLatch(20);
+        DBPoolTest.countDownLatch = countDownLatch;
+        AtomicInteger gets = new AtomicInteger(0);
+        AtomicInteger noGets = new AtomicInteger(0);
+        for (int i = 0;i < 20; i++){
+            new Thread(new Worker(50,gets,noGets), "thread_"+i).start();
+        }
+        countDownLatch.await();
+        System.out.println("一共获取的连接数："+1000);
+        System.out.println("获取失败的连接数："+noGets);
+        System.out.println("获取成功的连接数："+gets);
+    }
+}
