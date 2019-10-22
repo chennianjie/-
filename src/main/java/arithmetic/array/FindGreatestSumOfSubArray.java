29d311373ef9153db667c1e19130e9393cd8dd93
nianjie.chen <15663456952@163.com>
2019-10-22 17:54:57 +0800

    数组中连续子序列中的最大值

---
+++ src/main/java/arithmetic/array/FindGreatestSumOfSubArray.java
@@ -0,0 +1,30 @@
+package arithmetic.array;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * @Description:寻找一个数组中连续子序列中的最大值
+ * 思路：使用两个变量，一个用于存储当前值，一个用于存储最大值；
+ * 分两种情况考虑，当遍历的当前数是正数时更新最大值和当前值，当前数是负数时只更新当前值，再次遇到正数时再与最大值进行比较并更新
+ * eg: 1 2 3 -9 8    ===>   6
+ * @Author: nianjie.chen
+ * @Date: 10/22/2019
+ */
+public class FindGreatestSumOfSubArray {
+
+
+    public static void main(String[] args) {
+        FindGreatestSumOfSubArray findGreatestSumOfSubArray = new FindGreatestSumOfSubArray();
+        int[] array = {6,-3,-2,7,-15,1,2,2};
+
+    }
+
+    public int FindGreatestSumOfSubArray(int[] array) {
+
+
+        return 0;
+    }
+
+
+}
