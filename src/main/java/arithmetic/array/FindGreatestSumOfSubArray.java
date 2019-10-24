c357f2470109ac50b19955fb66d7eef4a0bae539
nianjie.chen <15663456952@163.com>
2019-10-24 17:56:34 +0800

    连续子序列最大值

--- src/main/java/arithmetic/array/FindGreatestSumOfSubArray.java
+++ src/main/java/arithmetic/array/FindGreatestSumOfSubArray.java
@@ -2,11 +2,13 @@ package arithmetic.array;
 
 /**
  * @Description:寻找一个数组中连续子序列中的最大值
- * 思路：使用两个变量，一个用于存储当前值，一个用于存储最大值；
- * 分两种情况考虑
- * 1.当加上当前数总数比当前数还要小的时候，总数和的起点从当前数开始
- * 2.当遍历的当前数是正数时更新最大值和当前值，当前数是负数时只更新当前值，再次遇到正数时再与最大值进行比较并更新
- * eg: 1 2 3 -9 8    ===>   6
+ * 思路：1 -2 3 10 -4 7 2 -5
+ *
+ * 当从1加到3时，总和为2，小于当前数3，也就是说，3比前面的子数组之和大，这样就舍弃3前面的子数组；
+ * 紧接着3 + 10 = 13，然后遇到一个负数，大家都知道，加上一个负数，总和变小，所以当前总和13可能为答案，保存；加到7的时候总和为13 + 7 - 4 = 16，所以更新最大总和为16，最终得出结果；
+ * 在此过程中，需要一个变量保存最大总和，一个变量保存实时总和值
+ *
+ *
  * @Author: nianjie.chen
  * @Date: 10/22/2019
  */
@@ -15,13 +17,19 @@ public class FindGreatestSumOfSubArray {
 
     public static void main(String[] args) {
         FindGreatestSumOfSubArray findGreatestSumOfSubArray = new FindGreatestSumOfSubArray();
-        int[] array = {6,-3,-2,7,-15,1,2,2};
-
+        int[] array = {1, -2, 3, 10, -4, 7, 2, -5};
+        int i = findGreatestSumOfSubArray.FindGreatestSumOfSubArray(array);
+        System.out.println(i);
     }
 
     public int FindGreatestSumOfSubArray(int[] array) {
+
+        if (array == null || array.length == 0) {
+            return 0;
+        }
+
         int currVal = 0;
-        int result = 0;
+        int sumMax = 0;
         //输入处理
         boolean flag = true;
         boolean flag2 = true;
@@ -37,24 +45,29 @@ public class FindGreatestSumOfSubArray {
         //如果flag为true，全部为正数；如果flag2为true，全部为负数
         if (flag) {
             for (int i : array) {
-                result += i;
+                sumMax += i;
             }
-            return result;
+            return sumMax;
         }
 
         if (flag2) {
-            return result;
+            return sumMax;
         }
 
-        for (int i : array) {
-            result = i + result;
-            if (i > 0 && result < i) {
-                result = i;
+        for (int i = 0; i < array.length; i++) {
+            if (currVal <= 0) {
+                sumMax = array[i];
             }
+            currVal += array[i];
+
+
+
+            if (currVal > sumMax) {
+                sumMax = currVal;
+            }
+
         }
-
-
-        return 0;
+        return sumMax;
     }
 
 
