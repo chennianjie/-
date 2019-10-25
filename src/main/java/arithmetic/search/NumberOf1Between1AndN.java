2b7090823bbdd3d11f44c689795b29b0da50b983
nianjie.chen <15663456952@163.com>
2019-10-25 17:54:49 +0800

    1 - n中 1出现的次数

---
+++ src/main/java/arithmetic/search/NumberOf1Between1AndN.java
@@ -0,0 +1,20 @@
+package arithmetic.search;
+
+/**
+ * @Description: 1 - n中 1出现的次数
+ * @Author: nianjie.chen
+ * @Date: 10/25/2019
+ */
+public class NumberOf1Between1AndN {
+
+    public int NumberOf1Between1AndN_Solution(int n) {
+
+        if (n <= 10) {
+            return 1;
+        }
+
+        //
+        return 0;
+    }
+
+}
