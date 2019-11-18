1d323fe57e02ac76e5c2b25d79b9c1150979990d
nianjie.chen <15663456952@163.com>
2019-11-18 16:18:40 +0800

    搜索二叉树中第k小的值

---
+++ src/main/java/arithmetic/tree/KthNodeValue.java
@@ -0,0 +1,31 @@
+package arithmetic.tree;
+
+import arithmetic.structure.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * @Description: 搜索二叉树中第k小的值
+ * @Author: nianjie.chen
+ * @Date: 11/18/2019
+ */
+public class KthNodeValue {
+    TreeNode KthNode(TreeNode pRoot, int k) {
+        if (pRoot != null) {
+            Stack<TreeNode> stack = new Stack<>();
+            while (!stack.isEmpty() || pRoot != null) {
+                if (pRoot != null) {
+                    stack.push(pRoot);
+                    pRoot = pRoot.left;
+                } else {
+                    pRoot = stack.pop();
+                    if (--k == 0) {
+                        return pRoot;
+                    }
+                    pRoot = pRoot.right;
+                }
+            }
+        }
+        return null;
+    }
+}
