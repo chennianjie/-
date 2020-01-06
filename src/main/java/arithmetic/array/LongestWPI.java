2a38113b5cf24ee4a2634e3600b87a5f811aab51
nianjie.chen <jaychen1@thomsonreuters.com>
2020-01-06 17:28:37 +0800

    表现良好的最大时间段

---
+++ src/main/java/arithmetic/array/LongestWPI.java
@@ -0,0 +1,14 @@
+package arithmetic.array;
+
+/**
+ * @Description: 表现良好的最大时间段
+ * @Author: nianjie.chen
+ * @Date: 1/6/2020
+ */
+public class LongestWPI {
+
+    public int longestWPI(int[] hours) {
+
+        return 0;
+    }
+}
